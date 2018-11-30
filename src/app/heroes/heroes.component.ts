589c5434dabf9e151081978a8d457771fa72199b
isaac.omollo <isaac.omollo@turnkeyafrica.com>
2018-11-30 08:18:58 +0300

    added a message for hero servise
    Signed-off-by: isaac.omollo <isaac.omollo@turnkeyafrica.com>

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -9,26 +10,22 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-constructor
-(private heroService: HeroService) {
-
-	}
-
- ngOnInit() {
-  this.getHeroes();
-}
-  
-  heroes: Hero[];
-  
   selectedHero: Hero;
 
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
-  
-getHeroes(): void {
-  this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-}
+  heroes: Hero[];
 
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
